159e80eb53db4de9c44a3e22c2dbbdabefb5d05c
yhtiee <99039727+yhtiee@users.noreply.github.com>
2023-01-15 15:10:01 +0100

    api url

--- src/context/API.jsx
+++ src/context/API.jsx
@@ -1,3 +1,3 @@
-let API_URL = "http://127.0.0.1:8000/"
-// let API_URL = "https://web-production-0dc8.up.railway.app/"
+// let API_URL = "http://127.0.0.1:8000/"
+let API_URL = "https://web-production-0dc8.up.railway.app/"
 export default API_URL
